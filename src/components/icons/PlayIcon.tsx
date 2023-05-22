3199f15ec05c696a9205375bd5ba6efa7f3fc086
vab1997 <victorbejas97@gmail.com>
2023-05-22 16:18:55 -0300

    Fix

--- src/components/icons/PlayIcon.tsx
+++ src/components/icons/PlayIcon.tsx
@@ -13,7 +13,7 @@ export function PlayIcon(
     >
       <path
         fill='currentColor'
-        fill-rule='evenodd'
+        fillRule='evenodd'
         d='M5.495 2.573 1.5.143C.832-.266 0 .25 0 1.068V5.93c0 .82.832 1.333 1.5.927l3.995-2.43c.673-.41.673-1.445 0-1.855'
       />
     </svg>
