6e04b99af275d0131bac8442b07eeee22f2bdb20
vab1997 <victorbejas97@gmail.com>
2023-05-25 23:33:58 -0300

    change styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,16 @@
-import Link from "next/link"
+import Link from 'next/link';
 
 export default function Header() {
   return (
     <header className='border-b border-t border-gray-800 flex items-center justify-center px-8 py-6 w-full'>
-      <div className="flex items-center justify-center gap-8 py-2 max-w-6xl w-full">
-        <div className="flex items-center justify-center gap-1 px-4">
-          <img src='/images/logo-developers.webp' className="w-24 h-auto" />
-          <span className="italic text-white text-md mt-[2px]">Developers</span>
-        </div>
-        <div className="flex w-full mt-[2px]">
-          <nav className="flex">
-            <ul className="flex items-center justify-center gap-4 [&>*]:text-white">
+      <div className='flex items-center justify-center gap-8 py-2 max-w-6xl w-full'>
+        <Link href='/' className='flex items-center justify-center gap-1 px-4'>
+          <img src='/images/logo-developers.webp' className='w-24 h-auto' />
+          <span className='italic text-white text-md mt-[2px]'>Developers</span>
+        </Link>
+        <div className='flex w-full mt-[2px]'>
+          <nav className='flex'>
+            <ul className='flex items-center justify-center gap-4 [&>*]:text-white'>
               <li>
                 <Link href='/' className='hover:opacity-70'>
                   Home
@@ -19,17 +19,20 @@ export default function Header() {
               <li>
                 <Link href='/documentation' className='hover:opacity-70'>
                   Documentation
-                </Link>    
+                </Link>
               </li>
             </ul>
           </nav>
         </div>
       </div>
       <div className='flex items-center justify-center'>
-        <Link href="/login" className="text-white bg-blue-600 rounded px-4 py-2 hover:opacity-90 hover:border-b-gray-500">
+        <Link
+          href='/login'
+          className='text-white bg-transparent border border-[#EE81C3] py-2 px-4 rounded hover:bg-[#EE81C3] hover:text-gray-50'
+        >
           Login
         </Link>
       </div>
-      </header>
-  )
+    </header>
+  );
 }
