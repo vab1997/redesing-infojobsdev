7cc21a3f55a499839f0d102e780e7fbd482785de
vab1997 <victorbejas97@gmail.com>
2023-05-25 23:34:08 -0300

    create file constants

---
+++ src/utils/constant.ts
@@ -0,0 +1,6 @@
+export const COLOR_METHOD = {
+  GET: 'bg-green-500/30 text-green-500',
+  PUT: 'bg-yellow-500/30 text-yellow-500',
+  POST: 'bg-blue-500/30 text-blue-500',
+  DELETE: 'bg-red-500/30 text-red-500',
+}
