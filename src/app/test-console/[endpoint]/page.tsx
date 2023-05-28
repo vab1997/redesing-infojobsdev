6c7455d53989f622eb932e8d6c581623f1ee910a
vab1997 <victorbejas97@gmail.com>
2023-05-28 18:42:00 -0300

    add responsive page test-console

--- src/app/test-console/[endpoint]/page.tsx
+++ src/app/test-console/[endpoint]/page.tsx
@@ -10,10 +10,10 @@ export default async function TestConsole({ params }: { params: { endpoint: stri
     <section className='flex items-center justify-center py-8 w-full'>
       <div className='flex flex-col items-center justify-center max-w-6xl w-full'>
         <section className='flex flex-col items-center justify-center gap-8 w-full'>
-          <h1 className='text-4xl font-bold'>
+          <h1 className='text-2xl md:text-4xl font-bold text-center'>
             Test Console for endpoint: {endpoint}
           </h1>
-          <div className='flex items-center flex-col gap-4 border border-[#5386AB] py-4 px-8 rounded w-3/4'>
+          <div className='flex items-center flex-col gap-4 border border-[#5386AB] py-4 px-8 rounded w-full lg:w-3/4'>
             <h3 className='font-medium text-[#5386AB] text-lg text-start w-full'>Operation</h3>
 
             <div className='flex items-center justify-center w-full'>
